5b6a25e08d8d51ae8675346d1a907a18c56e193b
Anand Jangid <jangidme88@gmail.com>
2023-03-24 17:24:29 +0530

    one button added

--- lib/main.dart
+++ lib/main.dart
@@ -68,6 +68,11 @@ class _MyHomePageState extends State<MyHomePage> {
             child: const Icon(Icons.add),
           ),
           const SizedBox(width: 10,),
+          FloatingActionButton(
+            onPressed: () => _bloc.counterEventSink.add(DecrementEvent()),
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
         ],
       ),
     );
